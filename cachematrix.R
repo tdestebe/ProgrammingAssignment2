3896c45a3abd55689744ebbf046d821f4d94aca3
tdestebe <thierry.estebenet@ge.com>
2016-02-09 17:16:22 +0100

    Initial version ready for submission
    First commit of assignment for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than computing it 
+## repeatedly.
+## In the following code is implemented a pair of functions that cache the 
+## inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+## TEST:
+## X <- diag(1:10, 10, 10) # Creates a diag matrix with values from 1 to 10.
+## XC <- makeCacheMatrix(X) # Assign the special diag "matrix" to XC
+## cacheSolve(XC)           # Calculates the inverse of X
+## cacheSolve(XC)           # get cached data
+## cacheSolve(XC) %*% X     # returns identity matrix from cached data 
